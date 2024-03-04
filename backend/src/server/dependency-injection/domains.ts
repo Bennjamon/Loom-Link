30752ca570887fd0f7de1250414b931dd2458f9d
Bennjamon <benjaminrivas500@gmail.com>
2024-03-03 21:41:15 -0300

    feature: Load user domain in server

--- backend/src/server/dependency-injection/domains.ts
+++ backend/src/server/dependency-injection/domains.ts
@@ -1,10 +1,12 @@
 import LinkDomain from "../../link/domain/Link.domain";
 import SharedDomain from "../../shared/domain/Shared.domain";
 import { Constructor } from "../../shared/types/Constructor";
+import UserDomain from "../../user/domain/User.domain";
 import BackendApplication from "../App";
 
 const domains: Constructor<any>[] = [
   SharedDomain,
+  UserDomain,
   LinkDomain,
   BackendApplication,
 ];
--- backend/src/user/domain/User.domain.ts
+++ backend/src/user/domain/User.domain.ts
@@ -1,5 +1,8 @@
 import Domain from "../../shared/decorators/Domain";
 import MongoDataSource from "../../shared/infrastructure/data-source/MongoDataSource";
+import UserController from "../controllers/User.controller";
+import UserRepository from "../repositories/User.repository";
+import UserService from "../services/User.service";
 
 @Domain({
   dependencies: [
@@ -13,6 +16,9 @@ import MongoDataSource from "../../shared/infrastructure/data-source/MongoDataSo
       class: MongoDataSource,
       args: ["user_sessions"],
     },
+    UserRepository,
+    UserService,
   ],
+  controllers: [UserController],
 })
 export default class UserDomain {}
